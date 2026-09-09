8bfc8073e9088124f3f54d6a5806b3b7144df86b
dev-ahad-ali <ahadali.dev@gmail.com>
2026-09-09 19:07:55 +0600

    Update: created get post and cookie routes

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,44 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response("Hello World!");
-	},
-} satisfies ExportedHandler<Env>;
+const PORT = 8080;
+
+const app = express();
+
+app.use(
+	cors({
+		origin: '*',
+	}),
+);
+
+app.use(cookieParser());
+
+app.get('/', (_, res) => {
+	res.json('hello from cloudflare express');
+});
+
+app.get('/health', (_, res) => {
+	res.json({
+		message: 'server is running successfully',
+	});
+});
+
+app.post('/echo', (req, res) => {
+	const message = req.body.messge;
+
+	res.json({
+		echo: message,
+	});
+});
+
+app.get('/cookie', (req, res) => {
+	const currentCookie = req.cookies.number;
+	let setCookie = currentCookie ? currentCookie + 1 : 1;
+
+	res.cookie('number', setCookie);
+});
+
+app.listen(PORT, () => {
+	console.log(`Server is listening or Port: ${PORT}`);
+});
